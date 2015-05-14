7b80c2fb5d0b26f6bc11ce07be1857755a9f6ce4
Adnan Zahid <adonztevez@ymail.com>
2015-05-14 15:58:21 +0500

    Some tweaking
    Some tweaking

--- config/auth.js
+++ config/auth.js
@@ -4,20 +4,20 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '805241509511410', // your App ID
-		'clientSecret' 	: '2e15efb281a31366bcd02bbbb49a0f7c', // your App Secret
+		'clientID' 		: '', // your App ID
+		'clientSecret' 	: '', // your App Secret
 		'callbackURL' 	: 'http://127.0.0.1/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey' 		: 'eXKtePAmiKU6BE3l1cSKXHoUJ',
-		'consumerSecret' 	: 'S5rQKZMIdb1qaPYLhMftQwCddHYaiuGXY4859lPNkEBPxUEQZ3',
+		'consumerKey' 		: '',
+		'consumerSecret' 	: '',
 		'callbackURL' 		: 'http://127.0.0.1/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
-		'clientID' 		: '441263314458-vattjc966pfg1gfl33t812at9lmd31v6.apps.googleusercontent.com',
-		'clientSecret' 	: 'Mzr4GZWy8yziiAKtwzN1f2Ks',
+		'clientID' 		: '',
+		'clientSecret' 	: '',
 		'callbackURL' 	: 'http://127.0.0.1/auth/google/callback'
 	}
 
